f1c1e5e4bfcca5fd56240ec1131b863062c86de8
Kenneth <kenneth.ng.nym@outlook.com>
2023-11-04 19:16:22 +0000

    Rename target name

--- Package.swift
+++ Package.swift
@@ -9,15 +9,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NanoPack",
-            targets: ["NanoPackSwift"]),
+            targets: ["NanoPack"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NanoPackSwift"),
+            name: "NanoPack"),
         .testTarget(
-            name: "NanoPackSwiftTests",
-            dependencies: ["NanoPackSwift"]),
+            name: "NanoPackTests",
+            dependencies: ["NanoPack"]),
     ]
 )
