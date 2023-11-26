a06b96c14d78082ee0bd4403e17f5cf81ba6fb25
Kenneth <kenneth.ng.nym@outlook.com>
2023-11-26 22:26:31 +0000

    Use relative indexing when reading data

--- Sources/NanoPack/NanoPack.swift
+++ Sources/NanoPack/NanoPack.swift
@@ -7,7 +7,7 @@ public typealias TypeID = Int
 public extension Data {
     /// Read the type ID of the message stored in the data buffer.
     func readTypeID() -> TypeID {
-        let id: Int32 = read(at: 0)
+        let id: Int32 = read(at: startIndex)
         return TypeID(id)
     }
     
@@ -15,14 +15,15 @@ public extension Data {
     ///
     /// - parameter at: The index of the first byte of the integer in the Data buffer.
     func read<T: FixedWidthInteger>(at index: Int) -> T {
-        return readUnaligned(at: index)
+        return readUnaligned(at: startIndex + index)
     }
     
     /// Read a fixed width integer at an unaligned position.
     ///
     /// - parameter at: The index of the first byte of the integer in the Data buffer.
     func readUnaligned<T: FixedWidthInteger>(at index: Int) -> T {
-        return subdata(in: index..<index + MemoryLayout<T>.size).withUnsafeBytes {
+        let offset = startIndex + index
+        return subdata(in: offset..<offset + MemoryLayout<T>.size).withUnsafeBytes {
             $0.load(as: T.self).littleEndian
         }
     }
@@ -33,7 +34,8 @@ public extension Data {
     /// - parameter withLength: The number of bytes of the string.
     ///                         Note that NanoPack strings are not null-terminated, so there is no need to include it in the length.
     func read(at index: Int, withLength: Int) -> String? {
-        return String(data: self[index..<index + withLength], encoding: .utf8)
+        let offset = startIndex + index
+        return String(data: self[offset..<offset + withLength], encoding: .utf8)
     }
     
     /// Read a boolean.
@@ -47,7 +49,8 @@ public extension Data {
     ///
     /// - parameter at: The index of the first byte of the double in the Data buffer.
     func read(at index: Int) -> Double {
-        return self[index..<index + MemoryLayout<Double>.size].withUnsafeBytes {
+        let offset = startIndex + index
+        return self[offset..<offset + MemoryLayout<Double>.size].withUnsafeBytes {
             $0.load(as: Double.self)
         }
     }
@@ -56,7 +59,7 @@ public extension Data {
     ///
     /// - parameter ofField: The number of the field.
     func readSize(ofField index: Int) -> Size {
-        let size: Int32 = readUnaligned(at: MemoryLayout<Int32>.size * (index + 1))
+        let size: Int32 = readUnaligned(at: MemoryLayout<Int32>.size * (startIndex + index + 1))
         return Size(size)
     }
 
@@ -64,7 +67,8 @@ public extension Data {
     ///
     /// - parameter at: The index of the first byte of the encoded size number.
     func readUnalignedSize(at index: Int) -> Int {
-        return subdata(in: index..<index + MemoryLayout<Int32>.size).withUnsafeBytes {
+        let offset = startIndex + index
+        return subdata(in: offset..<offset + MemoryLayout<Int32>.size).withUnsafeBytes {
             Size($0.load(as: Int32.self).littleEndian)
         }
     }
@@ -82,11 +86,12 @@ public extension Data {
     /// - parameter size: The size to be written
     /// - parameter at: The index at which the first byte of the encoded size should be
     mutating func write(size: Size, at index: Int) {
+        let offset = startIndex + index
         Swift.withUnsafeBytes(of: Int32(size).littleEndian) {
-            self[index] = $0[0]
-            self[index + 1] = $0[1]
-            self[index + 2] = $0[2]
-            self[index + 3] = $0[3]
+            self[offset] = $0[0]
+            self[offset + 1] = $0[1]
+            self[offset + 2] = $0[2]
+            self[offset + 3] = $0[3]
         }
     }
     
